0168d6852f29c54a7606d040b90a21d2de80547e
saadahmedbd <saadahmedbd0@gmail.com>
2024-10-08 00:17:29 +0600

    geenrate html for order summary

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,19 @@
-export const cart =[]; 
+export const cart =[{
+    //added cart object in default
+    //cart object have two elemnt that is product id and quentity
+    productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quentity:2
+},{
+    productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quentity:1
+}]; 
 
 //function of cartquentity
 export function addToCart(productId){
     let metchingItem ;
-      cart.forEach((item) =>{
-          if(productId === item.productId){
-              metchingItem=item;
+      cart.forEach((cartItem) =>{
+          if(productId === cartItem.productId){
+              metchingItem=cartItem;
           }
       });
       if(metchingItem){
